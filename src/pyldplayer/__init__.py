8aaa46fbbaefc0e7e947d65c9275d4f84315c2c1
ZackaryW <36378555+ZackaryW@users.noreply.github.com>
2024-11-22 09:55:58 -0800

    chore: cleanup

--- src/pyldplayer/__init__.py
+++
@@ -1,6 +0,0 @@
-from .core.config import ConfigCollection
-from .core.g import Global
-from .core.mapping import MappingCollection
-from .core.path import LDPath
-from .core.record import RecordCollection
-from .core.console import Console
--- src/pyldplayer/_internal/__init__.py
+++
@@ -1,3 +0,0 @@
-from typing import TypeVar, Union
-
-SOptional = Union[TypeVar("T"), None]
